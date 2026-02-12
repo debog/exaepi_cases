aa117ad081944714b1cf704b09f1bc8325f1c444
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2026-02-11 19:05:21 -0800

    improved job monitoring script:wq

--- multidisease/scripts/monitor.sh
+++ multidisease/scripts/monitor.sh
@@ -2,14 +2,12 @@
 
 for j in {1..1000000}; do
     clear
-    for i in .ensemble_bay*_${LCHOST}; do
-        echo $i
-        cat $i/*.out |grep "Run" |tail -n 6
-    done
-    echo ""
-    for i in .ensemble_CA*_${LCHOST}; do
-        echo $i
-        cat $i/*.out |grep "Run" |tail -n 6
+    for k in bay CA US; do
+        for i in .ensemble_${k}*_${LCHOST}; do
+            echo $i
+            cat $i/*.out |grep "Run" |tail -n 3
+        done
+        echo ""
     done
     sleep 2m
 done
