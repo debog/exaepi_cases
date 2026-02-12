9fd63aa68eb135ea87fc24fe62babedf67ae05f7
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2026-02-11 19:15:28 -0800

    added timestamp echo to monitor script

--- multidisease/scripts/monitor.sh
+++ multidisease/scripts/monitor.sh
@@ -2,6 +2,8 @@
 
 for j in {1..1000000}; do
     clear
+    echo "$(timedatectl | grep Local)"
+    echo ""
     for k in bay CA US; do
         for i in .ensemble_${k}*_${LCHOST}; do
             echo $i
